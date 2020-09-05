82791d487811e721f06b4b5e7df61c7130a7f9e2
Nian <1046783533@qq.com>
2020-09-05 21:02:56 +0800

    new website supported

--- blank.js
+++ blank.js
@@ -12,18 +12,52 @@
 
 // )
 
-obj = {
-    0: 'a',
-    1: 'b',
-    length: 2
-}
-console.log(obj.length)
+// obj = {
+//     0: 'a',
+//     1: 'b',
+//     length: 2
+// }
+// console.log(obj.length)
 
-obj.xxx = 'aaa'
-console.log(obj)
-console.log(obj.length)
+// obj.xxx = 'aaa'
+// console.log(obj)
+// console.log(obj.length)
 
-for (let key in obj) {
-    console.log('key', key)
-    console.log('keytype', typeof key)
-}
+// for (let key in obj) {
+//     console.log('key', key)
+//     console.log('keytype', typeof key)
+// }
+
+// a = 'aa'
+// b = 'bb'
+
+// var c = b.concat('a')
+// var c = b
+// console.log(c)
+// console.log(c === b)
+// c = 5
+// console.log(c)
+// console.log(b)
+
+
+// a = [1, 2]
+// var b = a.concat([])
+// console.log(b)
+// console.log(a === b)
+
+// const fileLines = [
+//     'Inspector Algar,Inspector Bardle,Mr. Barker,Inspector Barton',
+//     'Inspector Baynes,Inspector Bradstreet,Inspector Sam Brown',
+//     'Monsieur Dubugue,Birdy Edwards,Inspector Forbes,Inspector Forrester',
+//     'Inspector Gregory,Inspector Tobias Gregson,Inspector Hill',
+//     'Inspector Stanley Hopkins,Inspector Athelney Jones'
+// ];
+
+// console.log(fileLines[0].split('\''))
+
+// function splitLineReducer(acc, line) {
+//     return acc.concat(line.split('\,'));
+// }
+
+// const investigators = fileLines.reduce(splitLineReducer, []);
+// console.log(investigators);
